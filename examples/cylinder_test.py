0695569d50bf72c5a52f5350c6b702e18d1a2f06
Thomas Chaplin <tomrchaplin@gmail.com>
2023-04-11 16:13:57 +0100

    Format example

--- examples/cylinder_test.py
+++ examples/cylinder_test.py
@@ -2,53 +2,46 @@ from pprint import pprint
 from phimaker import compute_ensemble_cylinder
 
 domain = [
-        (0, []),
-        (0, []),
-        (0, []),
-        (0, []),
-        (1, [0,1]),
-        (1, [1,2]),
-        (1, [2,3]),
-        (1, [0,3]),
-        (10, [4,5,6,7])]
+    (0, []),
+    (0, []),
+    (0, []),
+    (0, []),
+    (1, [0, 1]),
+    (1, [1, 2]),
+    (1, [2, 3]),
+    (1, [0, 3]),
+    (10, [4, 5, 6, 7]),
+]
 
 codomain = [
-        (0, []),
-        (0, []),
-        (0, []),
-        (0, []),
-        (0.1, [0,1]),
-        (0.1, [1,2]),
-        (0.1, [2,3]),
-        (0.1, [0,3]),
-        (2, [0,2]),
-        (2, [4,5,8]),
-        (2, [6,7,8])]
+    (0, []),
+    (0, []),
+    (0, []),
+    (0, []),
+    (0.1, [0, 1]),
+    (0.1, [1, 2]),
+    (0.1, [2, 3]),
+    (0.1, [0, 3]),
+    (2, [0, 2]),
+    (2, [4, 5, 8]),
+    (2, [6, 7, 8]),
+]
 
-map = [
-        [0],
-        [1],
-        [2],
-        [3],
-        [4],
-        [5],
-        [6],
-        [7],
-        [9, 10]]
+map = [[0], [1], [2], [3], [4], [5], [6], [7], [9, 10]]
 
 ensemble, metadata = compute_ensemble_cylinder(domain, codomain, map)
 ker = ensemble.ker.paired
 
 diagrams = {
-        "f": ensemble.f,
-        "g": ensemble.g,
-        "rel": ensemble.rel,
-        "ker": ensemble.ker,
-        "im": ensemble.im,
-        "cok": ensemble.cok,
-        }
+    "f": ensemble.f,
+    "g": ensemble.g,
+    "rel": ensemble.rel,
+    "ker": ensemble.ker,
+    "im": ensemble.im,
+    "cok": ensemble.cok,
+}
 
-for dig_name,dig in diagrams.items():
+for dig_name, dig in diagrams.items():
     print(dig_name)
     for pair in dig.paired:
         t0 = metadata.times[pair[0]]
